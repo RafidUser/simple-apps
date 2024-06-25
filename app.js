407bf431a008fd5bcfeb106903b29fcc0ee39b12
rafid <salahudin.1702@gmail.com>
2024-06-25 13:27:52 +0700

    update app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.get('/Rafid', (req, res) => {
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
+    res.send{fields}
   })
 });
 
